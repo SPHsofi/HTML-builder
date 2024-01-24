600f1f58c368ec009e1ccf9388f2e689df5401dc
SPHsofi <Sofia_pash@mail.ru>
2024-01-24 21:37:27 +0300

    refactor: change function clearDirectory and copyDirectory to async/await and use promises

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,57 +4,45 @@ const path = require('path');
 const folderPath = path.join(__dirname, 'files');
 const pathNewFolder = path.join(__dirname, 'files-copy');
 
-function clearDirectory(source, target) {
-  fs.promises.access(target, fs.constants.F_OK)
-    .then(() => fs.promises.rm(target, { recursive: true }))
-    .then(() => {
-      copyDirectory(source, target);
-    })
-    .catch((err) => {
-      if (err.code === 'ENOENT') {
-        return fs.promises.mkdir(target, { recursive: true });
-      } else {
-        console.error('Ошибка очистки папки =>', err);
-      }
-    })
-    .catch((err) => {
-      console.error('Ошибка создания папки =>', err);
-    });
+async function clearDirectory(source, target) {
+  try {
+    await fs.promises.access(target, fs.constants.F_OK);
+    await fs.promises.rm(target, { recursive: true });
+  } 
+  catch (err) {
+    if (err.code !== 'ENOENT') {
+      console.error('Ошибка очистки папки =>', err);
+    }
   }
   
+  try {
+    await fs.promises.mkdir(target, { recursive: true });
+    await copyDirectory(source, target);
+  } 
+  catch (err) {
+    console.error('Ошибка создания папки =>', err);
+  }
+}
 
-function copyDirectory(source, target) {
-  fs.mkdir(target, { recursive: true }, (err) => {
-    if (err) {
-      console.error('Ошибка копирования папки =>', err);
-    } else {
-      fs.readdir(source, (err, files) => {
-        if (err) {
-          console.error('Ошибка чтения папки =>', err);
-        } else {
-          files.forEach((file) => {
-            const currentPath = path.join(source, file);
-            const targetPath = path.join(target, file);
-            fs.lstat(currentPath, (err, stat) => {
-              if (err) {
-                console.error('Ошибка получения статуса файла =>', err);
-              } else {
-                if (stat.isDirectory()) {
-                  copyDirectory(currentPath, targetPath);
-                } else {
-                  fs.copyFile(currentPath, targetPath, (err) => {
-                    if (err) {
-                      console.error('Ошибка копирования файлов =>', err);
-                    }
-                  });
-                }
-              }
-            });
-          });
-        }
-      });
+async function copyDirectory(source, target) {
+  try {
+    const files = await fs.promises.readdir(source);
+    for (const file of files) {
+      const currentPath = path.join(source, file);
+      const targetPath = path.join(target, file);
+      const stat = await fs.promises.lstat(currentPath);
+      
+      if (stat.isDirectory()) {
+        await copyDirectory(currentPath, targetPath);
+      } 
+      else {
+        await fs.promises.copyFile(currentPath, targetPath);
+      }
     }
-  });
+  } 
+  catch (err) {
+    console.error('Ошибка при копировании файлов=>', err);
+  }
 }
 
 clearDirectory(folderPath, pathNewFolder);
